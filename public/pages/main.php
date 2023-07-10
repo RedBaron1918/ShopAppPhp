a860f0f9c8169fdd40926a5a8393a1a1ffbe3d0a
giorgi.dat <datuashvili.giorgi@gmail.com>
2023-07-10 16:27:48 +0400

    refactor: Add pages for navigation.

--- public/pages/main.php
+++ public/pages/main.php
@@ -1,7 +1,4 @@
-
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="../styles/mainStyles.css">
 <main class="container">
-    This is MAIN page!
+    <?php include "./components/cards.php"; ?>
+
 </main>
