c2ea80702cf0f1fceb18c0054f1c449ef8f361d2
giorgi.dat <datuashvili.giorgi@gmail.com>
2023-07-10 16:21:10 +0400

    refactor: Combine all css styles to one css file because it helps to write styles faster. (Any comments are welcome).

--- public/layouts/header.php
+++ public/layouts/header.php
@@ -1,8 +1,23 @@
 
-        <?php
-        include "./layout/header.php";
-        include "./layout/footer.php";
 
+<!DOCTYPE html>
+<html lang="en">
 
-        
-        ?>
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./styles/navStyles.css">
+
+        <title>shop</title>
+</head>
+
+<header>
+<?php
+
+include "./components/navigation.php";
+?>
+</header>
+
+<body>
+
