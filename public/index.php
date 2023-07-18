1e2431c82a23e10417a74cf5d0e8311b5dbe292c
Misho Piradashvili <piradashvili.misho@gmail.com>
2023-07-18 20:18:50 +0400

    feat: added card, carousel

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once "../vendor/autoload.php";
 require_once "../src/database.php";
-
 include "./layouts/header.php";
 include "./components/carousel.php";
 include "./components/cards.php";
--- public/layouts/footer.php
+++ public/layouts/footer.php
@@ -2,5 +2,6 @@
 include_once "../public/components/footer_content.php";
 ?>
 </body>
-</html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="/ShopAppPhp/src/functions/js/carousel_script.js"></script>
+</html>
