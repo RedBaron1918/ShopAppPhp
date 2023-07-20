d9ea7347e1f4cde9a5ed89d8305cdfd91e63107a
giorgi.dat <datuashvili.giorgi@gmail.com>
2023-07-20 17:02:43 +0400

    style: chenge search bar visuals.

--- public/components/search.php
+++ public/components/search.php
@@ -1,8 +1,4 @@
-<?php
-
-?>
-
-<form class="d-flex" role="search">
-    <input class="form-control me-3 mb-3 " type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-secondary btnSearch btn-outline-warning" type="submit">Search</button>
+<form class="d-flex container   mt-3" role="search">
+    <input class="form-control search-input-border  me-3" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-secondary  search" type="submit">Search</button>
 </form>
