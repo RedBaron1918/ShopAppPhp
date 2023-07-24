8ebe91b72af54d3d54a102c6cb73a308192a6e71
RedBaron1918 <asatianikotiko1@gmail.com>
2023-07-24 17:38:32 +0400

    fix typo

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
-<?php 
-include "./layouts/header.php";?>
+<?php
+include "./layouts/header.php"; ?>
 <main class="container">
-<?php include "./components/cards.php";?>
+    <?php include "./components/cards.php"; ?>
 </main>
-<?php include "./layouts/footer.php";?>
-?>
+<?php include "./layouts/footer.php"; ?>
