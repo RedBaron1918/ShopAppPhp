08e43d1d896a2b3593cbd8f0dd991e679d261d21
RedBaron1918 <asatianikotiko1@gmail.com>
2023-07-27 01:00:32 +0400

    refactor:changed small stuff

---
+++ public/pages/home.php
@@ -0,0 +1,7 @@
+<?php
+require_once "../../vendor/autoload.php";
+require_once "../../src/database.php";
+include "../layouts/header.php";
+include "../components/carousel.php";
+include "../components/cards.php";
+include "../layouts/footer.php";
