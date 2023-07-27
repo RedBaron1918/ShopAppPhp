db514d6a71aee9732fdba5bf23dd1cbd9ce6782b
Misho Piradashvili <piradashvili.misho@gmail.com>
2023-07-28 01:39:37 +0400

    make cards

--- public/components/cards.php
+++ public/components/cards.php
@@ -11,14 +11,17 @@ $books = Product::all();
     ?>
       <div class="col-sm-12 col-md-6 col-lg-3 mb-3 mt-5 ">
         <div class="card align-items-center text-center border-0 shadow-lg p-4  img-parent ">
+          <div class="foto-heart ">
           <img src="<?= $book->img_url ?>" class="card-img-top rounded-circle card-image " alt="...">
-          <div class="card-body">
-            <h5 class="card-title "><?= "სახელი: " . $book->product_name; ?></h5>
-            <button type="button" class="btn "> <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi heart  " viewBox="0 0 16 16">
-              <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
+            <button type="button" class=" btn heart  " > <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi redButton  " viewBox="0 0 16 16">
+          <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
               </svg></button>
+          </div>
+           <div class="card-body ">
+            <h5 class="card-title "><?= "სახელი: " . $book->product_name; ?></h5>
+            
             <p class="card-text"><?= "ფასი: " .  $book->price ?></p>
-            <a href="#" class="btn btn-primary">ყიდვა</a>
+            <button class="btn-color">Add to cart</button>
           </div>
         </div>
       </div>
@@ -28,10 +31,33 @@ $books = Product::all();
 </div>
 
 
-<!-- <ul class="list-unstyled ">
-              <li class=""><a href="" class="= heart text-decoration-none ">
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-suit-heart  " viewBox="0 0 16 16">
-              <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
-              </svg>
-              </a></li>
-            </ul> -->
+<script>
+  function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+  }
+  
+  
+  const buttons = document.querySelectorAll('.btn-color');
+  
+  
+  buttons.forEach((button) => {
+    button.style.backgroundColor = getRandomColor();
+  });
+</script>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.btn ');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        button.style.backgroundColor = 'red';
+      });
+    });
+  });
+  
+</script>
+
--- public/layouts/footer.php
+++ public/layouts/footer.php
@@ -4,4 +4,5 @@ include_once  $_SERVER['DOCUMENT_ROOT'] . '/ShopAppPhp/public/components/footer_
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="/ShopAppPhp/src/functions/js/carousel_script.js"></script>
+
 </html>
